88403496ac2f08cf6a498ed3b825568edb21b418
abdfnx <abdfnabd@gmail.com>
2021-11-24 20:10:58 +0300

    add support for m1 macs #10

--- release.sh
+++ release.sh
@@ -12,6 +12,7 @@ fi
 
 rm -rf dist
 GOOS=darwin GOARCH=amd64 go build -o "dist/darwin-x86_64"
+GOOS=darwin GOARCH=arm64 go build -o "dist/darwin-arm64"
 GOOS=linux GOARCH=386 go build -o "dist/linux-i386"
 GOOS=linux GOARCH=amd64 go build -o "dist/linux-x86_64"
 GOOS=windows GOARCH=386 go build -o "dist/windows-i386"
