f72c8f7be2408e27770fade8cee275948e6f03b2
Abdfn <64256993+abdfnx@users.noreply.github.com>
2021-11-25 14:59:25 +0300

    update linux amd64 dist path (#19)

--- release.sh
+++ release.sh
@@ -14,7 +14,7 @@ rm -rf dist
 GOOS=darwin GOARCH=amd64 go build -o "dist/darwin-amd64"
 GOOS=darwin GOARCH=arm64 go build -o "dist/darwin-arm64"
 GOOS=linux GOARCH=386 go build -o "dist/linux-i386"
-GOOS=linux GOARCH=amd64 go build -o "dist/linux-x86_64"
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64"
 GOOS=windows GOARCH=386 go build -o "dist/windows-i386"
 GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64"
 
