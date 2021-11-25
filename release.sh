7e4ca01232c9dfab3a7de20bcf68faed2f1de9c1
Abdfn <64256993+abdfnx@users.noreply.github.com>
2021-11-25 13:54:18 +0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -16,6 +16,6 @@ GOOS=darwin GOARCH=arm64 go build -o "dist/darwin-arm64"
 GOOS=linux GOARCH=386 go build -o "dist/linux-i386"
 GOOS=linux GOARCH=amd64 go build -o "dist/linux-x86_64"
 GOOS=windows GOARCH=386 go build -o "dist/windows-i386"
-GOOS=windows GOARCH=amd64 go build -o "dist/windows-x86_64"
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64"
 
 gh release create $tag ./dist/* --title="${tag}" --notes "${tag}"
