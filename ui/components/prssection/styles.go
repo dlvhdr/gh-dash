e9119f2e049bd691c9d87b5868a3a00a9f3c593e
Dolev Hadar <6196971+dlvhdr@users.noreply.github.com>
2022-03-05 18:02:38 +0200

    fix ci column width (#91)

--- ui/components/prssection/styles.go
+++ ui/components/prssection/styles.go
@@ -3,8 +3,8 @@ package prssection
 import "github.com/charmbracelet/lipgloss"
 
 var (
-	ciCellWidth        = lipgloss.Width("CI")
-	linesCellWidth     = lipgloss.Width("123450 / -123450")
+	ciCellWidth        = lipgloss.Width(" CI ")
+	linesCellWidth     = lipgloss.Width(" 123450 / -123450 ")
 	updatedAtCellWidth = lipgloss.Width(" Updated")
 	prRepoCellWidth    = 20
 	prAuthorCellWidth  = 15
