77492e31a0008ecc3489bb5335e8e994123fb496
Cian Gallagher <cg1830@gmail.com>
2022-04-03 22:05:07 +0100

    feat: Add support for raspberry PI with ARM (#106)

--- release.sh
+++ release.sh
@@ -15,6 +15,8 @@ GOOS=darwin GOARCH=amd64 go build -o "dist/darwin-amd64"
 GOOS=darwin GOARCH=arm64 go build -o "dist/darwin-arm64"
 GOOS=linux GOARCH=386 go build -o "dist/linux-i386"
 GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64"
+GOOS=linux GOARCH=arm GOARM=6 go build -o "dist/linux-arm-6"
+GOOS=linux GOARCH=arm64 go build -o "dist/linux-arm64"
 GOOS=windows GOARCH=386 go build -o "dist/windows-i386.exe"
 GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64.exe"
 
