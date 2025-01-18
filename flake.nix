9494355ab31f69156bd6773e8574ecb57022588c
Dolev Hadar <dolevc2@gmail.com>
2025-01-18 23:44:05 +0200

    chore

--- flake.nix
+++ flake.nix
@@ -17,9 +17,14 @@
     in
     {
       devShell.${system} = pkgs.callPackage ./nix/devShell.nix { };
+
+      # this allows users to install gh-dash with nix
+      # by including gh-dash.packages.aarch64-darwin.default in their systemPackages
       packages.${system}.default = pkgs.callPackage ./nix/package.nix { };
+
+      # this allows?
       overlays.default = final: prev: {
-        ghostty = self.packages.${prev.system}.default;
+        gh-dash = self.packages.${prev.system}.default;
       };
     };
 }
