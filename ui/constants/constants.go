c629cdd17c0d92a25f5ac9894bc428ca8dada845
sideshowbarker <mike@w3.org>
2025-01-28 19:30:16 +0900

    feat: show issue/PR author-role indicators in UI, after author logins

--- ui/constants/constants.go
+++ ui/constants/constants.go
@@ -37,4 +37,11 @@ const (
 	MergedIcon  = ""
 	OpenIcon    = ""
 	ClosedIcon  = ""
+
+	NewContributorIcon = "" // \uebe9 nf-cod-verified_filled
+	ContributorIcon    = "" // \uedc6 nf-fa-user_check
+	CollaboratorIcon   = "" // \uedcf nf-fa-user_shield
+	MemberIcon         = "󰢏" // \udb82\udc8f nf-md-shield_account
+	OwnerIcon          = "󱇐" // \udb84\uddd0 nf-md-crown_outline
+	UnknownRoleIcon    = "󱐡" // \udb85\udc21 nf-md-incognito_circle
 )
