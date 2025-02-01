9b7cca61d8cf4c5d4db397688ba064ea4db42da0
Dolev Hadar <dolevc2@gmail.com>
2025-02-01 21:00:03 +0200

    fix: icons

--- Makefile
+++ Makefile
@@ -9,3 +9,11 @@ run-debug:
 .PHONY: test
 test:
 		go test -v ./...
+
+.PHONY: check-nerd-font
+check-nerd-font:
+	nerdfix check --quiet $$(fd --extension go)
+
+.PHONY: fix-nerd-font
+fix-nerd-font:
+	nerdfix fix --quiet --format=json $$(fd --extension go)
--- flake.nix
+++ flake.nix
@@ -6,40 +6,47 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
     {
       overlays.default = final: prev: {
         gh-dash = self.packages.${prev.system}.default;
       };
     }
-    // flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+    // flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      gh-dash = pkgs.buildGoModule {
-        pname = "gh-dash";
-        version = "v4.10.0";
-        src = ./.;
-        vendorHash = "sha256-lqmz+6Cr9U5IBoJ5OeSN6HKY/nKSAmszfvifzbxG7NE=";
-      };
-    in {
-      packages = {
-        default = gh-dash;
-        inherit gh-dash;
-      };
+        gh-dash = pkgs.buildGoModule {
+          pname = "gh-dash";
+          version = "v4.10.0";
+          src = ./.;
+          vendorHash = "sha256-lqmz+6Cr9U5IBoJ5OeSN6HKY/nKSAmszfvifzbxG7NE=";
+        };
+      in
+      {
+        packages = {
+          default = gh-dash;
+          inherit gh-dash;
+        };
 
-      devShells.default = pkgs.mkShell {
-        name = "gh-dash";
-        inherit (gh-dash) nativeBuildInputs buildInputs;
-        packages = with pkgs; [
-          gopls
-          golangci-lint
-          golangci-lint-langserver
-          goimports-reviser
-        ];
-      };
-    });
+        devShells.default = pkgs.mkShell {
+          name = "gh-dash";
+          inherit (gh-dash) nativeBuildInputs buildInputs;
+          packages = with pkgs; [
+            fd
+            goimports-reviser
+            golangci-lint
+            golangci-lint-langserver
+            gopls
+            nerdfix
+          ];
+        };
+      }
+    );
 }
