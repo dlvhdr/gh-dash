2ed47580214c4fd695a0392a27820ec75fd92504
Dolev Hadar <dolevc2@gmail.com>
2025-02-22 20:15:33 +0200

    build: nix

--- flake.nix
+++ flake.nix
@@ -2,9 +2,14 @@
   description = "gh-dash";
 
   inputs = {
+    nur.url = "github:nix-community/NUR";
+
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
+
+    caarlos0-nur.url = "github:caarlos0/nur";
+
   };
 
   outputs =
@@ -12,6 +17,8 @@
       self,
       nixpkgs,
       flake-utils,
+      nur,
+      caarlos0-nur,
     }:
     {
       overlays.default = final: prev: {
@@ -21,16 +28,33 @@
     // flake-utils.lib.eachDefaultSystem (
       system:
       let
+
+        # overlays allow extending and changing nixpkgs with custom packages
+        overlays = [
+          (final: prev: {
+            nur = import nur {
+              nurpkgs = prev;
+              pkgs = prev;
+              # add caarlos0 and charmbracelet modules to nixpkgs
+              repoOverrides = {
+                caarlos0 = import caarlos0-nur { pkgs = prev; };
+              };
+            };
+          })
+        ];
+
         pkgs = nixpkgs.legacyPackages.${system};
 
         gh-dash = pkgs.buildGoModule {
           pname = "gh-dash";
-          version = "v4.10.0";
+          version = "v4.12.0";
           src = ./.;
-          vendorHash = "sha256-lqmz+6Cr9U5IBoJ5OeSN6HKY/nKSAmszfvifzbxG7NE=";
+          vendorHash = "sha256-7s+Lp8CHo1+h2TmbTOcAGZORK+/1wytk4nv9fgD2Mhw=";
         };
       in
       {
+        pkgs.overlays = overlays;
+
         packages = {
           default = gh-dash;
           inherit gh-dash;
@@ -46,6 +70,7 @@
             golangci-lint-langserver
             gopls
             nerdfix
+            svu
             (callPackage ./docs { })
           ];
         };
