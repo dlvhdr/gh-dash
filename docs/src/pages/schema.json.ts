2877fe016ccd9b1d801320f33cf1fcccc4785ef0
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2026-06-18 22:39:18 -0500

    Fix name of builtin to selectSuggestion

--- docs/src/pages/schema.json.ts
+++ docs/src/pages/schema.json.ts
@@ -125,7 +125,7 @@ export function GET() {
               completions: [
                 {
                   key: "tab",
-                  builtin: "acceptSuggestion",
+                  builtin: "selectSuggestion",
                 },
               ],
             },
